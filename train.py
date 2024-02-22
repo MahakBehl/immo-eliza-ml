d337325607cccc01ab0119f913f9ec3d6d1333f7
Mahak Behl <mahakbehl@gmail.com>
2024-02-22 10:47:56 +0100

    update predict.py

--- train.py
+++ train.py
@@ -60,7 +60,7 @@ def train():
 
     # Train the model
     #model = LinearRegression()
-    model = RandomForestRegressor(n_estimators=100,max_depth=10,min_samples_split=2,min_samples_leaf=1,random_state=42)
+    model = RandomForestRegressor(n_estimators=100,max_depth=10,min_samples_split=2,min_samples_leaf=1,random_state=505)
     model.fit(X_train, y_train)
 
     # Evaluate the model
